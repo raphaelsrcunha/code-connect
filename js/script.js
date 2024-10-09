95a29a8c92971c39cb38b2f7b21db50f452500ef
Raphael Cunha <raphaelsrcunha@gmail.com>
2024-10-09 14:27:07 -0400

    code-connect first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const uploadButton = document.getElementById("upload-button");
+const inputUpload = document.getElementById("image-upload");
+
+uploadButton.addEventListener("click", () => {
+    inputUpload.click();
+})
